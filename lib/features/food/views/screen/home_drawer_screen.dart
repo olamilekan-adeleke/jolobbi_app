1897e0ac532a0ae6e81eaff5f073ec7d0d7f22cc
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-03-26 08:44:51 +0100

    added drawer item

--- lib/features/food/views/screen/home_drawer_screen.dart
+++ lib/features/food/views/screen/home_drawer_screen.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:jolobbi_app/features/food/views/widgets/drawer/drawer_item_wigdet.dart';
 
-import '../../../../cores/components/custom_text_widget.dart';
-import '../../../../cores/components/image_widget.dart';
 import '../../../../cores/constants/color.dart';
 import '../../../../cores/utils/sizer_utils.dart';
 import '../widgets/drawer/drawer_user_profile.dart';
@@ -17,9 +17,45 @@ class HomeDrawerWidget extends StatelessWidget {
       child: Column(
         children: <Widget>[
           const DrawerUserProfileWidget(),
+          verticalSpace(40),
+          DrawerItemWidget(
+            icon: CupertinoIcons.person,
+            title: 'Profile',
+            onTap: () {},
+          ),
+          verticalSpace(20),
+          DrawerItemWidget(
+            icon: Icons.store_mall_directory_outlined,
+            title: 'See All Vendor',
+            onTap: () {},
+          ),
+          verticalSpace(20),
+          DrawerItemWidget(
+            icon: Icons.find_in_page_outlined,
+            title: 'Order History',
+            onTap: () {},
+          ),
+          verticalSpace(20),
+          DrawerItemWidget(
+            icon: Icons.location_on_outlined,
+            title: 'Track Order',
+            onTap: () {},
+          ),
+          verticalSpace(20),
+          DrawerItemWidget(
+            icon: Icons.info_outline_rounded,
+            title: 'Help',
+            onTap: () {},
+          ),
+          const Spacer(),
+          DrawerItemWidget(
+            icon: Icons.exit_to_app,
+            title: 'Logout',
+            onTap: () {},
+          ),
+          // verticalSpace(40),
         ],
       ),
     );
   }
 }
-
---
+++ lib/features/food/views/widgets/drawer/drawer_item_wigdet.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../cores/components/custom_text_widget.dart';
+import '../../../../../cores/constants/color.dart';
+import '../../../../../cores/utils/sizer_utils.dart';
+
+class DrawerItemWidget extends StatelessWidget {
+  const DrawerItemWidget({
+    Key? key,
+    required this.icon,
+    required this.title,
+    this.onTap,
+  }) : super(key: key);
+
+  final IconData icon;
+  final String title;
+  final Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: <Widget>[
+            Icon(icon, size: sp(15), color: kcWhite),
+            horizontalSpace(),
+            TextWidget(
+              title,
+              fontSize: sp(15),
+              fontWeight: FontWeight.w400,
+              textColor: kcWhite,
+            ),
+          ],
+        ),
+        // const Divider(color: kc),
+      ],
+    );
+  }
+}
