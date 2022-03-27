0e894657c8d0788a29f15b577ea296f07c94087f
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-03-27 11:57:40 +0100

    update

--- functions/src/controllers/compare_pin.js
+++ functions/src/controllers/compare_pin.js
@@ -1,9 +1,12 @@
 const bcrypt = require("bcrypt");
+const functions = require("firebase-functions");
+const admin = require("../../firebase_admin_helper");
 
 const comparePinAndUpdate = async (oldPin, currentHashedPin, newPin) => {
-  const oldPinHashed = bcrypt.hashSync(oldPin, 10);
+  //   const oldPinHashed = bcrypt.hashSync(oldPin, 10);
 
-  const result = bcrypt.compareSync(oldPinHashed, currentHashedPin);
+  const result = bcrypt.compareSync(oldPin, currentHashedPin);
+  //   functions.logger.log(`old: ${oldPinHashed} | new: ${currentHashedPin}`);
 
   if (result == false) {
     throw { code: 400, msg: "Old Pin Does Not Match!" };
--- functions/src/http_function/update_wallet_pin.js
+++ functions/src/http_function/update_wallet_pin.js
@@ -1,15 +1,18 @@
-const comparePinAndUpdate = require("../controllers/compare_pin");
 const functions = require("firebase-functions");
+const comparePinAndUpdate = require("../controllers/compare_pin");
+const getUserPinById = require("../controllers/get_user_pin_by_id");
 
 const updateUserWalletPinFunction = async (req, res) => {
   try {
     const { oldPin, newPin, userId } = req.body;
 
-    if (pin.length < 4 || !userId) {
+    if (oldPin.length < 4 || newPin.length < 4 || !userId) {
       throw { code: 400, msg: "Bad Request Body!" };
     }
 
-    const userPinData = await getUserPinById();
+    const userPinData = await getUserPinById(userId);
+
+    functions.logger.log(userPinData);
 
     await comparePinAndUpdate(oldPin, userPinData.pin, newPin);
 
