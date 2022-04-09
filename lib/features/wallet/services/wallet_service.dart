6316bcfb401ecdff06ce08a853d7358dd1d01dbd
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-04-09 19:54:00 +0100

    added WalletService

---
+++ lib/features/wallet/services/wallet_service.dart
@@ -0,0 +1,12 @@
+import 'package:jolobbi_app/features/wallet/model/wallet_data_model.dart';
+import 'package:jolobbi_app/features/wallet/repository/wallet_repository.dart';
+
+class WalletService {
+  static final WalletRepository _walletRepository = WalletRepository();
+
+  Future<WalletDataModel> getWalletData() async {
+    Map<String, dynamic> data = await _walletRepository.getWalletBalance();
+
+    return WalletDataModel.fromMap(data);
+  }
+}
