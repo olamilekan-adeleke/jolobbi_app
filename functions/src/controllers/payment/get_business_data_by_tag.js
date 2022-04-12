cde1de0f612f6aa334f5ff9bd64209e645450c1a
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-04-12 15:18:49 +0100

    added getVendorDataByTag

---
+++ functions/src/controllers/payment/get_business_data_by_tag.js
@@ -0,0 +1,18 @@
+const admin = require("../../../firebase_admin_helper");
+
+const getVendorDataByTag = async (vendorTag) => {
+  const snapshot = await admin
+    .firestore()
+    .collection("vendors")
+    .where("vendorTag", "==", vendorTag)
+    .limit(1)
+    .get();
+
+  if (snapshot.empty) {
+    throw { code: 400, msg: "No Vendor Was Found With The Tag " + vendorTag };
+  }
+
+    return snapshot[0].data();
+};
+
+module.exports = getVendorDataByTag;
--- functions/src/http_function/payment/transafer_to_business_by_tag.js
+++ functions/src/http_function/payment/transafer_to_business_by_tag.js
@@ -5,10 +5,11 @@ const transferToBusinessByTag = async (req, res) => {
     const { businessTag, userId } = req.body;
 
     // get bussiness data by bussiness tag
+    const vendorData = await getVendorDataByTag(businessTag);
 
-    
+    // make transfer from user wallet to vendor wallet
 
-
+    res.status(200).json({ status: "success", msg: "Transfer Successful!" });
   } catch (error) {
     functions.logger.error(error);
 
