bd5e8807c94eed929dc1a6196b74bd037a9aead8
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-04-19 16:47:30 +0100

    added monify paymnet

--- lib/features/wallet/views/widgets/fund_wallet_amount_widget.dart
+++ lib/features/wallet/views/widgets/fund_wallet_amount_widget.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:monnify_flutter_sdk/monnify_flutter_sdk.dart';
 import 'package:uuid/uuid.dart';
 
 import '../../../../cores/components/custom_button.dart';
@@ -81,30 +82,38 @@ class FundWalletAmountWidget extends StatelessWidget {
       );
     }
 
-    final Map<String, dynamic>? paymentResponse =
-        await AppRouter.instance.navigate(
-      FlutterWeb(
-        amount: _fundWalletCubit.state.amount,
-        transactionRef: "jolobbi_food-${const Uuid().v1()}",
-        description: "Fund Wallet",
-        user: profileCubit.state.userData!,
+    TransactionResponse transactionResponse =
+        await MonnifyFlutterSdk.initializePayment(
+      Transaction(
+        2000,
+        "NGN",
+        profileCubit.state.userData!.name,
+        profileCubit.state.userData!.email,
+        "jollobi-${DateTime.now().millisecondsSinceEpoch}",
+        "Description of payment",
+        metaData: {
+          // TODO: add more metadata
+          "ip": "196.168.45.22",
+          "device": "mobile_flutter"
+        },
+        paymentMethods: [PaymentMethod.CARD, PaymentMethod.ACCOUNT_TRANSFER],
       ),
     );
 
-    log(paymentResponse.toString());
+    log(transactionResponse.toMap().toString());
 
-    if (paymentResponse == null) {
-      AppRouter.instance.goBack();
+    // if (paymentResponse == null) {
+    //   AppRouter.instance.goBack();
 
-      return SnackBarService.showWarningSnackBar(
-        context: context,
-        message: 'Opps, it seems like something went wrong!',
-      );
-    }
+    //   return SnackBarService.showWarningSnackBar(
+    //     context: context,
+    //     message: 'Opps, it seems like something went wrong!',
+    //   );
+    // }
 
-    // {status: successful, customer: {name: Jack The Ripper , email: teduvf@gmail.com, phone_number: 07052936789}, transaction_id: 3280243, tx_ref: jolobbi_food-9f61f1a0-b8cf-11ec-9613-e18c12f05117, flw_ref: FLW-M03K-df672cc4e6e41ea7cc1016820faff81a, currency: NGN, amount: 200}
+    // // {status: successful, customer: {name: Jack The Ripper , email: teduvf@gmail.com, phone_number: 07052936789}, transaction_id: 3280243, tx_ref: jolobbi_food-9f61f1a0-b8cf-11ec-9613-e18c12f05117, flw_ref: FLW-M03K-df672cc4e6e41ea7cc1016820faff81a, currency: NGN, amount: 200}
 
-    _fundWalletCubit.fundWallet(paymentResponse['transaction_id']);
-    AppRouter.instance.goBack();
+    // _fundWalletCubit.fundWallet(paymentResponse['transaction_id']);
+    // AppRouter.instance.goBack();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:jolobbi_app/app.dart';
+import 'package:monnify_flutter_sdk/monnify_flutter_sdk.dart';
 
 import 'features/authentication/repository/push_notification_repo.dart';
 
@@ -14,6 +15,12 @@ Future<void> main() async {
       await Firebase.initializeApp();
 
       await PushNotificationService().initNotification();
+
+      MonnifyFlutterSdk.initialize(
+        'MK_TEST_BM0J9L4PF8',
+        '5496050632',
+        ApplicationMode.TEST,
+      );
 
       runApp(const JolobbiApp());
     },
