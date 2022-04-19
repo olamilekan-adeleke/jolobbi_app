3a7b52c5ff9320c80958e04f3ad157781d828d29
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-04-19 23:42:13 +0100

    update

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,12 @@
 const functions = require("firebase-functions");
+const webHookHandlerFunction = require("./src/controllers/payment/web_hook_handler");
 const createUserWalletPinFunction = require("./src/http_function/create_wallet_pin");
 const transferToBusinessByTag = require("./src/http_function/payment/transafer_to_business_by_tag");
 const verifyPaymentFunction = require("./src/http_function/payment/verify_payment");
 const updateUserWalletPinFunction = require("./src/http_function/update_wallet_pin");
 const verifyWalletPinFunction = require("./src/http_function/wallet_pin/verify_pin");
 const onNewUserCreatedFunction = require("./src/trigger_function/on_new_user_created");
+
 require("dotenv").config();
 
 // http functions
@@ -22,7 +24,7 @@ exports.verifyWalletPin = functions.https.onRequest(verifyWalletPinFunction);
 // TODO: deprecated functions
 exports.verifyPayment = functions.https.onRequest(verifyPaymentFunction);
 
-exports.monnifyWebHook = functions.https.onRequest();
+exports.monnifyWebHook = functions.https.onRequest(webHookHandlerFunction);
 
 exports.transferToBusinessByTag = functions.https.onRequest(
   transferToBusinessByTag
---
+++ functions/src/controllers/payment/web_hook_handler.js
@@ -0,0 +1,3 @@
+const webHookHandlerFunction = async () => {};
+
+module.exports = webHookHandlerFunction;
